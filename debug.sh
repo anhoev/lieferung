5b1ad5ca7c45082cf5cba1ed1decf3a2d31d0ae7
tran <tran@silpion.de>
2016-11-28 17:45:54 +0100

    update

--- debug.sh
+++ debug.sh
@@ -1,3 +1,4 @@
 git commit -a -m 'update'
 git push
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://192.168.1.12:4444/kill
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://192.168.1.12:4444/debug
