45a257fa37aeffaa8796f6494171948d39566275
tran <tran@silpion.de>
2016-11-28 17:41:06 +0100

    update

--- backend/indexDebug.js
+++ backend/indexDebug.js
@@ -20,6 +20,8 @@ cms.menu = {
 
 cms.server('backend/en', '');
 
+let nodeServer;
+
 cms.app.get('/debug', function *(req, res) {
     process.chdir(require('path').resolve(__dirname, '../'));
     let _out = '';
@@ -28,13 +30,16 @@ cms.app.get('/debug', function *(req, res) {
         _out += out.toString() + '\n';
         console.log(out.toString());
         return _out;
-        out = require('child_process').execSync('npm test', 'utf-8');
-        _out += out.toString();
+        nodeServer = require('child_process').execSync('npm test', 'utf-8');
+        _out += nodeServer.toString();
         console.log(out.toString());
     } catch (e) {
         console.warn(e);
     }
+})
 
+cms.app.get('/kill', function *(req, res) {
+    if (nodeServer) nodeServer.kill();
 })
 
 // cms.data.online.wsAddress = 'ws://localhost:8888';
---
+++ debug.sh
@@ -0,0 +1,3 @@
+git commit -a -m 'update'
+git push
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://192.168.1.12:4444/debug
