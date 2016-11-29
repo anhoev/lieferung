f6cc83a3977242d87bb244f56f65b718108a8c5f
tran <tran@silpion.de>
2016-11-29 23:47:01 +0100

    update

--- backend/kasse.js
+++ backend/kasse.js
@@ -230,7 +230,7 @@ const Report = cms.registerSchema({
             $scope.delete = function (_export) {
                 cms.execServerFn('Report', $scope.model, 'delete', _export).then(function () {
                     _export.deleted = true;
-                    $scope.refresh();
+                    $scope.refresh(true);
                 });
             }
 
