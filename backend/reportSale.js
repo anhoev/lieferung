217f1503af6b1168132c744cdcfeb55f1ee5a349
tran <tran@silpion.de>
2016-12-10 20:16:29 +0100

    update

--- backend/reportSale.js
+++ backend/reportSale.js
@@ -67,7 +67,6 @@ const ReportSale = cms.registerSchema({
                 waiting();
                 cms.execServerFn('ReportSale', $scope.model, 'report', $scope.data.from, $scope.data.to, $scope.data.type).then(function ({data}) {
                     window._waitingModal.close();
-                    debugger
                     $scope.groups = JsonFn.parse(data, true);
                 })
             }
